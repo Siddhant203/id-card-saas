32c20a1174639e94adfe3156739641bb0828131b
Siddhant_203 <siddhant835229.basia@gmail.com>
2026-04-30 12:21:22 +0530

    Added blogs

--- app.py
+++ app.py
@@ -80,6 +80,25 @@ def about():
 def privacy():
     return render_template('privacy.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/disclaimer')
+def disclaimer():
+    return render_template('disclaimer.html')
+
+@app.route('/pvc-print-guide')
+def pvc_print_guide():
+    return render_template('blogs/pvc-print-guide.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == "__main__":
     app.run()
